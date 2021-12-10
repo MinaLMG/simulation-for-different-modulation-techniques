1d3188da48cabacbbd0bde574137a5762811451a
minaLMG <beandro6@gmail.com>
2021-12-10 05:57:00 +0200

    tested the final project

--- project.py
+++ project.py
@@ -22,11 +22,11 @@ def neg_m_plus_val(x):
 
 
 def AM_DSB_LC(x):
-    val = 5*np.cos(100000*2*np.pi*x)*(3+x)
+    val = 5*np.cos(100000*2*np.pi*x)*(3+m(x))
     return val
 
 def AM_DSB_SC(x):
-    val = 5*np.cos(100000*2*np.pi*x)*(x)
+    val = 5*np.cos(100000*2*np.pi*x)*(m(x))
     return val
 
 def FM(x,kf):
@@ -37,55 +37,59 @@ def PM(x,kp):
     val = 5*np.cos(100000*2*np.pi*x+kp*m(x))
     return val
 
-N = pow(10,7)
-T = 20/N
-x_values = np.linspace(0,20, N)
+def AM_SSB_SC(x):
+    val = 5*1.5/2*np.cos(2*np.pi*(100000+2000)*x)
+    return val
+
+N = pow(10,3)
+T = 1/1000/N
+x_values = np.linspace(0,1/1000, N)
 y_values = m(x_values)
-# fig1, ax1 = plt.subplots()
-# plt.plot(x_values, y_values, label="m(t)")
-# plt.title("functions in time domain")
-# plt.xlabel("time (s)")
-# plt.ylabel("amplitude (v)")
-# plt.legend()
-# plt.tight_layout()
+fig1, ax1 = plt.subplots()
+plt.plot(x_values, y_values, label="m(t)")
+plt.title("functions in time domain")
+plt.xlabel("time (s)")
+plt.ylabel("amplitude (v)")
+plt.legend()
+plt.tight_layout()
 
 ######### large carrier #############
 
-# y_values_AM_DSB_LC = AM_DSB_LC(x_values)
-# fig2, ax2 = plt.subplots()
-# plt.plot(x_values, y_values_AM_DSB_LC, label="(m(t)+3)cos(wct)")
-# plt.title("functions in time domain")
-# plt.xlabel("time (s)")
-# plt.ylabel("amplitude (v)")
-# plt.legend()
-# plt.tight_layout()
-# plt.plot(x_values,y_values_AM_DSB_LC,label="m(t)AM_DSB_LC")
+y_values_AM_DSB_LC = AM_DSB_LC(x_values)
+fig2, ax2 = plt.subplots()
+plt.plot(x_values, y_values_AM_DSB_LC, label="(m(t)+3)cos(wct)")
+plt.title("functions in time domain")
+plt.xlabel("time (s)")
+plt.ylabel("amplitude (v)")
+plt.legend()
+plt.tight_layout()
+plt.plot(x_values,y_values_AM_DSB_LC,label="m(t)AM_DSB_LC")
 
 ######### suppressed carrier #############
 
-# y_values_AM_DSB_SC = AM_DSB_LC(x_values)
-# fig3, ax3 = plt.subplots()
-# plt.plot(x_values, y_values_AM_DSB_SC, label="(m(t)+3)cos(wct)")
-# plt.title("functions in time domain")
-# plt.xlabel("time (s)")
-# plt.ylabel("amplitude (v)")
-# plt.legend()
-# plt.tight_layout()
-# plt.plot(x_values,y_values_AM_DSB_SC,label="m(t)AM_DSB_SC")
+y_values_AM_DSB_SC = AM_DSB_SC(x_values)
+fig3, ax3 = plt.subplots()
+plt.plot(x_values, y_values_AM_DSB_SC, label="m(t)cos(wct)")
+plt.title("functions in time domain")
+plt.xlabel("time (s)")
+plt.ylabel("amplitude (v)")
+plt.legend()
+plt.tight_layout()
+plt.plot(x_values,y_values_AM_DSB_SC,label="m(t)AM_DSB_SC")
 
 ######### frequency modulation #############
 
-# y_values_FM = FM(x_values,1000)
-# fig4, ax4 = plt.subplots()
-# plt.plot(x_values, y_values_FM, label="ac(cos(wct+kf/wc*sin(wct))")
-# plt.title("functions in time domain")
-# plt.xlabel("time (s)")
-# plt.ylabel("amplitude (v)")
-# plt.legend()
-# plt.tight_layout()
-# plt.plot(x_values,y_values_FM,label="m(t)FM")
+y_values_FM = FM(x_values,1000)
+fig4, ax4 = plt.subplots()
+plt.plot(x_values, y_values_FM, label="ac(cos(wct+kf/wc*sin(wct))")
+plt.title("functions in time domain")
+plt.xlabel("time (s)")
+plt.ylabel("amplitude (v)")
+plt.legend()
+plt.tight_layout()
+plt.plot(x_values,y_values_FM,label="m(t)FM")
 
-######### frequency modulation #############
+######### phase modulation #############
 
 y_values_PM = PM(x_values,10)
 fig5, ax5 = plt.subplots()
@@ -96,59 +100,85 @@ plt.ylabel("amplitude (v)")
 plt.legend()
 plt.tight_layout()
 plt.plot(x_values,y_values_PM,label="m(t)PM")
+
+######### single side band modulation #############
+
+y_values_AM_SSB_SC = AM_SSB_SC(x_values)
+fig6, ax6 = plt.subplots()
+plt.plot(x_values, y_values_AM_SSB_SC, label="ac*am/2*cos((wc+or-wm)t)")
+plt.title("functions in time domain")
+plt.xlabel("time (s)")
+plt.ylabel("amplitude (v)")
+plt.legend()
+plt.tight_layout()
+plt.plot(x_values,y_values_AM_SSB_SC,label="m(t)SSB_SC")
+
+
+
 ########### frequency part #############
 
 ########### original signal ############
-# figf1, axf1 = plt.subplots()
-# y_values_f = fft(y_values)
-# w = blackman(N)
-# ywf = fft(y_values*w)
-# xf = fftfreq(N, 20/N)[:N//2]
-# plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
-# # plt.semilogy(xf[1:N//2], 2.0/N * np.abs(ywf[1:N//2]), '-r')
-# plt.legend(['FFT'])
-# plt.show()
+figf1, axf1 = plt.subplots()
+y_values_f = fft(y_values)
+w = blackman(N)
+ywf = fft(y_values*w)
+xf = fftfreq(N, 1/1000/N)[:N//2]
+plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
+# plt.semilogy(xf[1:N//2], 2.0/N * np.abs(ywf[1:N//2]), '-r')
+plt.legend(['FFT original signal'])
+plt.show()
 
 ######### large carrier #############
 
-# figf2, axf2 = plt.subplots()
-# y_values_f = fft(y_values_AM_DSB_LC)
-# w = blackman(N)
-# ywf = fft(y_values_AM_DSB_LC*w)
-# xf = fftfreq(N, 20/N)[:N//2]
-# plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
-# plt.legend(['FFT'])
-# plt.show()
+figf2, axf2 = plt.subplots()
+y_values_f = fft(y_values_AM_DSB_LC)
+w = blackman(N)
+ywf = fft(y_values_AM_DSB_LC*w)
+xf = fftfreq(N, 1/1000/N)[:N//2]
+plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
+plt.legend(['FFT large carrier'])
+plt.show()
 
 ######### suppressed carrier #############
 
-# figf3, axf3 = plt.subplots()
-# y_values_f = fft(y_values_AM_DSB_SC)
-# w = blackman(N)
-# ywf = fft(y_values_AM_DSB_SC*w)
-# xf = fftfreq(N, 20/N)[:N//2]
-# plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
-# plt.legend(['FFT'])
-# plt.show()
+figf3, axf3 = plt.subplots()
+y_values_f = fft(y_values_AM_DSB_SC)
+w = blackman(N)
+ywf = fft(y_values_AM_DSB_SC*w)
+xf = fftfreq(N, 1/1000/N)[:N//2]
+plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
+plt.legend(['FFT suppressed carrier'])
+plt.show()
 
 ######### frequency modulation #############
 
-# figf4, axf4 = plt.subplots()
-# y_values_f = fft(y_values_FM)
-# w = blackman(N)
-# ywf = fft(y_values_FM*w)
-# xf = fftfreq(N, 20/N)[:N//2]
-# plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
-# plt.legend(['FFT'])
-# plt.show()
+figf4, axf4 = plt.subplots()
+y_values_f = fft(y_values_FM)
+w = blackman(N)
+ywf = fft(y_values_FM*w)
+xf = fftfreq(N, 1/1000/N)[:N//2]
+plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
+plt.legend(['FFT frequency modulation'])
+plt.show()
 
-######### frequency modulation #############
+######### phase modulation #############
 
 figf5, axf5 = plt.subplots()
 y_values_f = fft(y_values_PM)
 w = blackman(N)
 ywf = fft(y_values_PM*w)
-xf = fftfreq(N, 20/N)[:N//2]
+xf = fftfreq(N, 1/1000/N)[:N//2]
 plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
-plt.legend(['FFT'])
+plt.legend(['FFT phase modulation'])
+plt.show()
+
+######### single side band modulation #############
+
+figf6, axf6 = plt.subplots()
+y_values_f = fft(y_values_AM_SSB_SC)
+w = blackman(N)
+ywf = fft(y_values_AM_SSB_SC*w)
+xf = fftfreq(N, 1/1000/N)[:N//2]
+plt.semilogy(xf[1:N//2], 2.0/N * np.abs(y_values_f[1:N//2]), '-b')
+plt.legend(['FFT single side band modulation'])
 plt.show()
